a3a59422f7b702bea9c82af3afff66e209eec5c8
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-23 19:31:24 +0600

    in memory user details manager implemented

--- src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.joyonta.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +23,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password("password")
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+          annaSmithUser
+        );
     }
 }
