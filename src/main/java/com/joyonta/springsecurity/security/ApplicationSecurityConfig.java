ef54d1e79d22722bdca2944351be7ac778cfb8e8
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-25 18:11:30 +0600

    disabled: csrf, http basic authentication .... enabled: form login

--- src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
@@ -31,14 +31,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Override
