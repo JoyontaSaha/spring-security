ad19c34c5baaf59425d410b3af7f21554c876688
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-25 19:31:39 +0600

    remember me cookies and other options customized

--- src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.joyonta.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -41,7 +43,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe();
+                .rememberMe().tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                .key("somethingverysecured");
     }
 
     @Override
