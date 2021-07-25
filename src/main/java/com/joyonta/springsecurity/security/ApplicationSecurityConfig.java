f9801b5d808118dcc08aec3b432046d9e5122fe6
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-25 18:43:38 +0600

    custom log in page implemented

--- src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/joyonta/springsecurity/security/ApplicationSecurityConfig.java
@@ -37,7 +37,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
